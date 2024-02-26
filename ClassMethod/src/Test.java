5b4c89fbb0b58354c73b7a89a02eaa093f1a172b
Laura Tita <lauratita316@gmail.com>
2024-02-26 08:41:46 +0700

    sabo first

---
+++ ClassMethod/src/Test.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Neendy's
+ */
+public class Test{
+    
+//    OUTPUT 1
+//    private int i = 0;
+//    
+//    class TestInner {
+//        public int i = 1;
+//        void sayHi(){
+//            System.out.println(i);
+//        }
+//    }
+//    
+//    public static void main(String[] args){
+//    TestInner = class
+//    inner = object
+//    new = instansiasi
+//        TestInner inner = new Test().new TestInner();
+//        inner.sayHi();
+//    }
+    
+//    OUTPUT ERROR
+//    public int count(int i){
+//        return 0;
+//    }
+//    
+//    public int count(int j){ // terjadi error karena nama method double dan tipe data yang sama
+//        return 0;
+//    }
+    
+    
+//    OUTPUT Hello
+//class TestInner{
+//    public void print(){
+//        System.out.println("Hi");
+//    }
+//}
+//
+//public class Test {
+//    public static void main(String[] args){
+//        TestInner t = new TestInner(){
+//            public void print(){
+//                System.out.println("Hello");
+//            }
+//        };
+//        t.print();
+//    }
+    
+    
+//    OUTPUT Integer
+//    public void doNothing(Object object){
+//        System.out.println("Object");
+//    }
+//    
+//    public void doNothing(Integer i){
+//        System.out.println("Integer");
+//    }
+//    
+//    public static void main(String[] args){
+//        Test test = new Test();
+//        test.doNothing(null);
+//    }
+    
+    
+    
+    
+}
